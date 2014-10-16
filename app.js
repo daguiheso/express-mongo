8001858d0d52e7e25566f2ea2dabb36d367a15b6
Daniel <danielhernandez16@hotmail.com>
2014-10-16 05:41:25 -0700

    Creando Proyecto

---
+++ app.js
@@ -0,0 +1,41 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var errorHandler = require('errorhandler');
+
+var app = express();
+
+// modulos
+var home = require('./controllers/home');
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/layouts');
+app.set('view engine', 'jade');
+app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(methodOverride());
+//app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(errorHandler());
+}
+
+// rutas
+app.use(home);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
